b0b77ccdaa69e4de314dd63168c4f3a4abf6fec3
Renato Avellar <o.onizuka@icloud.com>
2023-09-12 21:00:09 -0300

    aula-02

---
+++ server/download.js
@@ -0,0 +1,20 @@
+import ytdl from 'ytdl-core'
+import fs from 'fs'
+
+
+export const download = (videoId) =>{
+  const videoURL = "https://www.youtube.com/shorts/" + videoId
+  console.log('Realizando o download do vídeo: ', videoId)
+  ytdl(videoURL, {quality: "lowestaudio", filter: "audioonly"})
+  .on("info", (info)=>{
+    const seconds = info.formats[0].approxDurationMs / 1000
+    if (seconds > 60){
+      throw new Error('Esse vídeo não é um shorts')
+    }
+  }).on('end', ()=>{
+    console.log('Download finalizado')
+  })
+  .on("error", (error)=>{
+    console.log("Não foi possível fazer o download do video. Detalhes do erro: ", error)
+  }).pipe(fs.createWriteStream('./tmp/audio.mp4'))
+}
